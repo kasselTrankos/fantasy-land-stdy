180e838b5b206f4a66ad0f15b413032412b3c80f
kasselTrankos <alvrao.touzon@gmail.com>
2019-06-12 11:02:27 +0200

    add dependecies as the tuto

--- Multiply.js
+++ Multiply.js
@@ -1,12 +1,32 @@
 const FL = require('fantasy-land');
+// modo js prototype
+// function Multiply(value) {
+//   if(!(this instanceof Multiply)) return new Multiply(value);
+//   this._value = value;
+// }
+// //  Sum.fantasy-land/empty :: () -> Sum
+// Multiply[FL.empty] = function() { return Multiply (1); };
 
-function Multiply(value) {
-  if(!(this instanceof Multiply)) return new Multiply(value);
-  this._value = value;
-}
-//  Sum.fantasy-land/empty :: () -> Sum
-Multiply[FL.empty] = function() { return Multiply (1); };
+// Multiply.prototype[FL.concat] = function (a) {
+//   return new Multiply(a._value * this._value);
+// }
 
-Multiply.prototype[FL.concat] = function (a) {
-  return new Multiply(a._value * this._value);
+class Multiply {
+  constructor(value){
+    this.value = value;
+  }
+  // ***  Monoid ADT : debe cumplir empty, y concat
+  // antasy-land/empty
+  static [FL.empty]() { return new Multiply(1); };
+  // fantasy-land/concat
+  [FL.concat](a) {
+    return new Multiply(a.value * this.value);
+  }
+  
+  inspect() {
+    return this.value;
+  }
 }
+
+
+module.exports = {Multiply};
--- test/multiply.spec.js
+++ test/multiply.spec.js
@@ -1,8 +1,12 @@
 const {expect} = require('chai');
+const FL = require('fantasy-land');
 
-const Multiply = require('./../Multiply'); 
+const {Multiply} = require('./../Multiply'); 
 describe('Multiply', function () {
- it('should return number of charachters in a string', function () {
-    expect('hello').to.be.equal('pop');
+ it('Expect Fantasy land concat works ....', () => {
+    const seven = new Multiply(7);
+    const twelve = new Multiply(12);
+    expect(seven[FL.concat](twelve).value).to.be.equal(84);
   });
+
 });
