59b83a72cafe8c569a1019518966757bbb409da7
kasselTrankos <alvrao.touzon@gmail.com>
2019-06-13 09:16:56 +0200

    trying to add Semigroup

--- Multiply.js
+++ Multiply.js
@@ -1,32 +1,54 @@
 const FL = require('fantasy-land');
+const Z = require ('sanctuary-type-classes');
 // modo js prototype
-// function Multiply(value) {
-//   if(!(this instanceof Multiply)) return new Multiply(value);
-//   this._value = value;
-// }
-// //  Sum.fantasy-land/empty :: () -> Sum
-// Multiply[FL.empty] = function() { return Multiply (1); };
-
-// Multiply.prototype[FL.concat] = function (a) {
-//   return new Multiply(a._value * this._value);
-// }
-
-class Multiply {
-  constructor(value){
-    this.value = value;
-  }
-  // ***  Monoid ADT : debe cumplir empty, y concat
-  // antasy-land/empty
-  static [FL.empty]() { return new Multiply(1); };
-  // fantasy-land/concat
-  [FL.concat](a) {
-    return new Multiply(a.value * this.value);
-  }
-  
-  inspect() {
-    return this.value;
-  }
+function Multiply(value) {
+  if(!(this instanceof Multiply)) return new Multiply(value);
+  this.value = value;
 }
+//  Sum.fantasy-land/empty :: () -> Sum
+Multiply[FL.empty] = function() { return Multiply (0); };
+
+Multiply.prototype[FL.concat] = function (a) {
+  return Multiply(a.value * this.value);
+};
+//  Multiply#fantasy-land/invert :: Multiply ~> () -> Multiply
+Multiply.prototype[FL.invert] = function () {
+  return Multiply(0);
+};
+Multiply.prototype[FL.equals] = function (a) {
+  return Z.equals (this.value, a.value);
+};
+// Multiply.prototype[FL.compose] = function (a) {
+//   const Multiply = this;
+//   return function(b) {return a(Multiply(b));};
+// };
+// function Function$prototype$compose(other) {
+//   var semigroupoid = this;
+//   return function(x) { return other (semigroupoid (x)); };
+// }
+
+// class Multiply {
+//   constructor(value){
+//     this.value = value;
+//   }
+//   // ***  Monoid ADT : debe cumplir empty, y concat
+//   // antasy-land/empty
+//   [FL.empty]() { return new Multiply(1); };
+//   // fantasy-land/concat
+//   [FL.concat](a) {
+//     return new Multiply(a.value * this.value);
+//   }
+//   [FL.equals](a) {
+//     return Z.equals (this.value, a.value);
+//   }
+//   [FL.invert]() {
+//     return new Multiply(1/this.value);
+//   }
+  
+//   inspect() {
+//     return this.value;
+//   }
+// }
 
 
 module.exports = {Multiply};
--- test/multiply.spec.js
+++ test/multiply.spec.js
@@ -1,12 +1,27 @@
 const {expect} = require('chai');
+const _  = require('lodash');
 const FL = require('fantasy-land');
+const laws = require('fantasy-laws');
+const jsc = require ('jsverify');
+const show = require ('sanctuary-show');
+const Z = require ('sanctuary-type-classes');
 
 const {Multiply} = require('./../Multiply'); 
-describe('Multiply', function () {
- it('Expect Fantasy land concat works ....', () => {
-    const seven = new Multiply(7);
-    const twelve = new Multiply(12);
-    expect(seven[FL.concat](twelve).value).to.be.equal(84);
-  });
-
+const MultiplyArb = jsc.number.smap(Multiply, multiply => multiply.value, show);
+const {associativity}= laws.Semigroup(Z.equals, Multiply);
+// const {leftInverse, rightInverse}= laws.Group(Z.equals, Multiply);
+// const {leftIdentity, rightIdentity}= laws.Monoid(Z.equals, Multiply);
+// console.log(laws.Monoid(Z.equals, Multiply));
+// const testReflexivity = laws.Setoid.reflexivity(Z.equals, Multiply);
+// console.log(laws.Setoid.reflexivity(Z.equals, Multiply));
+// const testLeftInverse = leftInverse (MultiplyArb);
+// const testRightInverse = rightInverse (MultiplyArb);
+// const testRightIdentity = rightIdentity (MultiplyArb);
+const testAssociativity = associativity (MultiplyArb);
+// const testAssociativity = associativity (MultiplyArb);
+describe('Multiply => ',  () => {
+  // it('testRightInverse', testRightInverse);
+  // it('testLeftInverse', testLeftInverse);
+  // it('testRightIdentity', testRightIdentity);
+  it('testAssociativity', testAssociativity);
 });
