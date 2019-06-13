78604f2eb50991c8b1702645bee45cfaa10a90fd
kasselTrankos <alvaro.touzon@gmail.com>
2019-06-13 10:55:00 +0200

    Multiply is a monoid

--- Multiply.js
+++ Multiply.js
@@ -1,54 +1,33 @@
-const FL = require('fantasy-land');
-const Z = require ('sanctuary-type-classes');
-// modo js prototype
-function Multiply(value) {
-  if(!(this instanceof Multiply)) return new Multiply(value);
-  this.value = value;
-}
-//  Sum.fantasy-land/empty :: () -> Sum
-Multiply[FL.empty] = function() { return Multiply (0); };
+'use strict';
+const {empty, of, concat, equals} = require('fantasy-land');
+const {tagged} = require('daggy');
+const  Multiply = tagged('value');
 
-Multiply.prototype[FL.concat] = function (a) {
-  return Multiply(a.value * this.value);
+
+Multiply[of] = value => Multiply(value);
+Multiply[empty] = () => Multiply(1);
+
+Multiply.prototype[equals] = function(y) {
+    return this.value === y.value;
 };
-//  Multiply#fantasy-land/invert :: Multiply ~> () -> Multiply
-Multiply.prototype[FL.invert] = function () {
-  return Multiply(0);
+Multiply.prototype[concat] = function(y) {
+    return Multiply(this.value * y.value);
 };
-Multiply.prototype[FL.equals] = function (a) {
-  return Z.equals (this.value, a.value);
-};
-// Multiply.prototype[FL.compose] = function (a) {
-//   const Multiply = this;
-//   return function(b) {return a(Multiply(b));};
-// };
-// function Function$prototype$compose(other) {
-//   var semigroupoid = this;
-//   return function(x) { return other (semigroupoid (x)); };
-// }
-
-// class Multiply {
-//   constructor(value){
-//     this.value = value;
-//   }
-//   // ***  Monoid ADT : debe cumplir empty, y concat
-//   // antasy-land/empty
-//   [FL.empty]() { return new Multiply(1); };
-//   // fantasy-land/concat
-//   [FL.concat](a) {
-//     return new Multiply(a.value * this.value);
-//   }
-//   [FL.equals](a) {
-//     return Z.equals (this.value, a.value);
-//   }
-//   [FL.invert]() {
-//     return new Multiply(1/this.value);
-//   }
-  
-//   inspect() {
-//     return this.value;
-//   }
-// }
 
 
-module.exports = {Multiply};
+
+// Multiply[of] = value => Multiply(value)
+// // modo js prototype
+// //  Sum.fantasy-land/empty :: () -> Sum
+// Multiply[empty] = () => Multiply (1);
+
+// Multiply.prototype[concat] = function(that) {
+//   return Multiply(that.value * this.value);
+// } 
+// //  Multiply#fantasy-land/invert :: Multiply ~> () -> Multiply
+// // Multiply.prototype[FL.invert] = () => Multiply(0);
+// Multiply.prototype[equals] = that =>  this.value === that.value;
+
+
+
+module.exports = Multiply;
--- test/multiply.spec.js
+++ test/multiply.spec.js
@@ -1,27 +1,16 @@
 const {expect} = require('chai');
-const _  = require('lodash');
-const FL = require('fantasy-land');
+const {concat} = require('fantasy-land');
 const laws = require('fantasy-laws');
 const jsc = require ('jsverify');
 const show = require ('sanctuary-show');
 const Z = require ('sanctuary-type-classes');
 
-const {Multiply} = require('./../Multiply'); 
+const Multiply = require('./../Multiply'); 
 const MultiplyArb = jsc.number.smap(Multiply, multiply => multiply.value, show);
-const {associativity}= laws.Semigroup(Z.equals, Multiply);
-// const {leftInverse, rightInverse}= laws.Group(Z.equals, Multiply);
-// const {leftIdentity, rightIdentity}= laws.Monoid(Z.equals, Multiply);
-// console.log(laws.Monoid(Z.equals, Multiply));
-// const testReflexivity = laws.Setoid.reflexivity(Z.equals, Multiply);
-// console.log(laws.Setoid.reflexivity(Z.equals, Multiply));
-// const testLeftInverse = leftInverse (MultiplyArb);
-// const testRightInverse = rightInverse (MultiplyArb);
-// const testRightIdentity = rightIdentity (MultiplyArb);
-const testAssociativity = associativity (MultiplyArb);
-// const testAssociativity = associativity (MultiplyArb);
+const {leftIdentity, rightIdentity} = laws.Monoid(Z.equals, Multiply);
+const testRightIdentity = rightIdentity (MultiplyArb);
+const testLeftIdentity = leftIdentity (MultiplyArb);
 describe('Multiply => ',  () => {
-  // it('testRightInverse', testRightInverse);
-  // it('testLeftInverse', testLeftInverse);
-  // it('testRightIdentity', testRightIdentity);
-  it('testAssociativity', testAssociativity);
+  it('testRightIdentity', testRightIdentity);
+  it('testLeftIdentity', testLeftIdentity);
 });
