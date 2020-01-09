6eaea885688ad4ed7b8cb3dfed5a8259fcc59cea
kasselTrankos <alvaro.touzon@gmail.com>
2020-01-09 22:34:57 +0100

    remove dr boolean

--- test/apply.spec.js
+++ test/apply.spec.js
@@ -31,55 +31,6 @@ describe('Apply => ',  () => {
 
   });
   it('manual container', ()=> {
-    class Maybe {
-        static of(x) {
-          return new Maybe(x);
-        }
-      
-        get isNothing() {
-          return this.$value === null || this.$value === undefined;
-        }
-      
-        get isJust() {
-          return !this.isNothing;
-        }
-      
-        constructor(x) {
-          this.$value = x;
-        }
-      
-        ap(f) {
-          return this.isNothing ? this : f.map(this.$value);
-        }
-      
-        chain(fn) {
-          return this.map(fn).join();
-        }
-      
-        inspect() {
-          return this.isNothing ? 'Nothing' : `Just(${inspect(this.$value)})`;
-        }
-      
-        getType() {
-          return `(Maybe ${this.isJust ? getType(this.$value) : '?'})`;
-        }
-      
-        join() {
-          return this.isNothing ? this : this.$value;
-        }
-      
-        map(fn) {
-          return this.isNothing ? this : Maybe.of(fn(this.$value));
-        }
-      
-        sequence(of) {
-          return this.traverse(of, x => x);
-        }
-      
-        traverse(of, fn) {
-          return this.isNothing ? of(this) : fn(this.$value).map(Maybe.of);
-        }
-      }
       const add = x => y => x + y;
       Container = function(x) {
         this.x = x;
@@ -93,9 +44,6 @@ describe('Apply => ',  () => {
       Container.prototype.ap = function(o) {
         return o.map(this.x)
       }
-    //   console.log(Container.of(2).map(x => x + 9), '00000000')
     console.log(Container.of(add).ap(Container.of(1113)).ap(Container.of(1113)));
-    console.log(Maybe.of(a=>b=>c=> a + b + c).ap(Maybe.of(2)).ap(Maybe.of(2)).ap(Maybe.of(2)))
-    //   console.log(Container.of(2).map(add).ap(Container.of(3)));
   });
 });
